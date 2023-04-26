bdbf04b3fc2fc391ff9cc1deef0ee92a6b8bca56
izaac wilkowski <im.izaac.seo@gmail.com>
2023-04-25 22:50:53 -0700

    Monospace and working config

--- init.el
+++ init.el
@@ -2,16 +2,18 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+	      ("org" . "https://orgmode.org/elpa/")
+	      ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+(package-refresh-contents t)
 
 ;; handle non-linux
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+;; Force evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -29,7 +31,7 @@
 
 ;; build new UI
 ;; 	font
-(set-face-attribute 'default nil :font "iMonolegible" :height 150)
+(set-face-attribute 'default nil :font "liberation mono" :height 125)
 
 (load-theme 'wombat)
 
@@ -50,6 +52,17 @@
 
 ;; Make ESC leave prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(package-selected-packages
+   '(treemacs-evil swiper cmake-mode use-package ivy doom-modeline command-log-mode)))
 
 ;; deps
 (use-package swiper) ;; for ivy
@@ -114,15 +127,64 @@
 	     ([remap describe-command] . helpful-command)
 	     ([remap describe-variable] . counsel-describe-variable)
 	     ([remap describe-key] . helpful-key))
+(use-package general
+	     :config
+	     (general-create-definer rune/leader-keys
+				     :keymaps '(normal insert visual emacs)
+				     :prefix "SPC"
+				     :global-prefix "C-SPC")
+	     (rune/leader-keys
+	       "t" 	'(:ignore t :which-key "toggles")
+	       "tt" 	'(counsel-load-theme :which-key "choose theme")))
+(use-package evil
+	     :init
+	     (setq evil-want-integration t)
+	     (setq evil-want-keybinding nil)
+	     (setq evil-want-C-u-scroll t)
+	     (setq evil-want-C-i-jump nil)
+	     :config
+	     (evil-mode 1)
+	     (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+
+	     ;; Use visual line motions even outside of visual line mode buffers
+	     (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+	     (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+	     (evil-set-initial-state 'messages-bugger-mode 'normal)
+	     (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil magit
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Documents/code")
+    (setq projectile-project-search-path '("~/Documents/code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+   "ts" '(hydra-text-scale/body :which-key "scale text"))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(swiper cmake-mode use-package ivy doom-modeline command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
